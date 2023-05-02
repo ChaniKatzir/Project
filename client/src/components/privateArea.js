cc0d33957c98762ea7e767d4fc5ffb6253e1176a
chaniKatzir <chani76076@gmail.com>
2023-05-02 15:49:53 +0300

    1

--- client/src/components/privateArea.js
+++ client/src/components/privateArea.js
@@ -16,19 +16,26 @@ const Area = (props) => {
   const [person, setPerson] = useState(null);
   const [p, setP] = useState();
   const [s, setS] = useState();
-  const [ins, setIns] = useState([]);
-  var x = []
+  const [ins, setIns] = useState();
+  const [times, setTimes] = useState(0);
+  let values,keys,dataP,dataS;
+  let x;
   var obj;
 
   const insts = async (x, id) => {
-    if (context.status === 3) {
-      x = await getData("institute", person["person.id_institute_student"]);
+    if (person) {
+      let z;
+      if (context.status === 3) {
+
+        z = await getData(`institute/${person["id_institute_student"]}`);
+      }
+      else {
+        let y = person["id_institute_staff"];
+        z = await getData(`institute/${y}`);
+        console.log(z);
+      }
+      setIns(z)
     }
-    else {
-      x = await getData("institute", person["person.id_institute_staff"]);
-    }
-    // console.log("ins", ins);
-    setIns(x)
   }
 
   const st = async (x, body) => {
@@ -36,18 +43,18 @@ const Area = (props) => {
     return a;
   }
 
-  const role = (id) => {
-    if (id === 1) return ("מנהל ראשי")
-    else if (id === 2) return ("צוות")
-    return ("תלמיד")
-  }
+  // const role = (id) => {
+  //   if (id === 1) return ("מנהל ראשי")
+  //   else if (id === 2) return ("צוות")
+  //   return ("תלמיד")
+  // }
 
   const findp = async (x, id) => {
     if (context.status === 3) {
       let p = await st("student", { "id_person": context.id });
+      console.log();
       if (p) {
-        console.log("p[pppp", p[0]);
-        setPerson(p)
+        setPerson(p[0])
       }
     }
     else {
@@ -58,67 +65,86 @@ const Area = (props) => {
     }
   }
 
-  useEffect(() => { findp(); console.log("stsart"); }, []);
-
-  useEffect(() => {
-    if (person !== null) {
+  useEffect(() => { 
+if(times==0)  {
+  findp() 
+}}, []);
+ 
+useEffect(() => {
+    if (person) {
       insts();
-      
-       obj = {
-        "שם פרטי:": person["person.first_name"],
-        "שם משפחה:": person["person.last_name"],
-        "כתובת:": person["person.address"],
-        "טלפון:": person["person.phone_number"],
-        "פלאפון:": person["person.password"],
-        "מייל:": person["person.Email"],
-        "פרטי בנק:": person["person.bank_account"],
-        "סיסמה:": person["person.password"],
-        "סטטוס:": person["person.status_person"]
-      }
-      setP(obj);
     }
   }, [person]);
 
+useEffect(()=>{
+  if (ins) {
+    obj = {
+      "שם פרטי:": person["person.first_name"],
+      "שם משפחה:": person["person.last_name"],
+      "כתובת:": person["person.address"],
+      "טלפון:": person["person.phone_number"],
+      "פלאפון:": person["person.password"],
+      "מייל:": person["person.Email"],
+      "פרטי בנק:": person["person.bank_account"],
+      "סיסמה:": person["person.password"],
+      "סטטוס:": person["person.status_person"]
+    }
+    console.log("immmmmmmmmm hearrrrrrr");
+    setP(obj);
+  }
+},[ins])
   useEffect(() => {
-    if (ins.length > 0) {
+
+    if (p) {
+      console.log(p,"pppppgggggggggggggggg");
+
       if (context.status == 3) {
         let s = [
-          { "שכר לימוד": person[0].tuition },
-          { "שנתון:": person[0].yearbook },
+          { "שכר לימוד": person.tuition },
+          { "שנתון:": person.yearbook },
           { "שם מוסד:": ins.name }
         ]
-        console.log("s", s);
+        console.log("ssssss", s);
         setS(s);
       }
       else {
+        
         let s = [
-          { "תפקיד:": role(person[0].id_role) },
-          { "שנות וותק:": person[0].seniority },
+          { "תפקיד:": "צוות" },
+          { "שנות וותק:": person.seniority },
           { "שם מוסד:": ins.name }
         ]
-        console.log("s", s);
+        console.log("ssssss", s);
         setS(s);
       }
-      console.log("ins", ins);
     }
-  }, [ins]);
+  }, [p]);
 
   return (
-    p?<>
+    s ? <>{console.log(p,"paaaaaaaaaaaaa")};
+   {/* { values=Object.values(p)
+      keys=Object.keys(p)
+
+      values.forEach((key, index) =>
+      dataP.push(key + " :" + values[index]));
+      values=Object.values(s)
+      keys=Object.keys(s)
+      values.forEach((key, index) =>
+        dataS.push(key + " :" + values[index]));} */}
       {
         context.status == 3 ? <><Menue status={props.status} id={props.id} arr={["בית", "איזור אישי"]} icon={["pi pi-fw pi-home", "pi pi-fw pi-book"]} navigate={["/Home", "/PrivateArea"]} />
           <Card p={p} s={s} title={"איזור אישי"} />
           <Button label="לקבלת דווח נוכחות" rounded /></>
 
-        : context.status == 2 ? <><Menue status={props.status} id={props.id} arr={["בית", "תלמידים", "איזור אישי", "ניהול תוכן"]} icon={["pi pi-fw pi-home", "pi pi-fw pi-pencil", "pi pi-fw pi-book", "pi pi-paperclip"]} navigate={["/Home", "/Student", "/PrivateArea", "/MaterialManagement"]} />
-          <Card p={p} s={s} title={"איזור אישי"} />
-          <Button label="לקבלת דווח נוכחות" rounded /></>
-
-          : <><Menue status={props.status} id={props.id} arr={["בית", "ניהול חשבונות", "תלמידים", "צוות", "ניהול תוכן", "הגדרות מוסד"]} icon={["pi pi-fw pi-home", "pi pi-fw pi-calendar", "pi pi-fw pi-pencil", "pi pi-fw pi-users", "pi pi-paperclip", "pi-cog"]} navigate={["/Home", "/AccountManagement", "/Student", "/Staff", "/MaterialManagement", "/definitions"]} />
-            <Card p={p} s={s} title={"איזור אישי"} />
+          : context.status == 2 ? <><Menue status={props.status} id={props.id} arr={["בית", "תלמידים", "איזור אישי", "ניהול תוכן"]} icon={["pi pi-fw pi-home", "pi pi-fw pi-pencil", "pi pi-fw pi-book", "pi pi-paperclip"]} navigate={["/Home", "/Student", "/PrivateArea", "/MaterialManagement"]} />
+            <Card p={p[0]} s={s[0]} title={"איזור אישי"} />
             <Button label="לקבלת דווח נוכחות" rounded /></>
-      }</>
-    :<div>no person</div>
-      )
+
+            : <><Menue status={props.status} id={props.id} arr={["בית", "ניהול חשבונות", "תלמידים", "צוות", "ניהול תוכן", "הגדרות מוסד"]} icon={["pi pi-fw pi-home", "pi pi-fw pi-calendar", "pi pi-fw pi-pencil", "pi pi-fw pi-users", "pi pi-paperclip", "pi-cog"]} navigate={["/Home", "/AccountManagement", "/Student", "/Staff", "/MaterialManagement", "/definitions"]} />
+              <Card p={p} s={s} title={"איזור אישי"} />
+              <Button label="לקבלת דווח נוכחות" rounded /></>
+     } </>
+      : <div>no person</div>
+  )
 };
 export default Area
